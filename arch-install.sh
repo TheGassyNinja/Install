349d5c115f6dedb1e45aadeace7a9bb699d0a9e6
TheGassyNinja <compujohn96@gmail.com>
2022-07-10 18:50:19 -0500

    update script

--- arch-install.sh
+++ arch-install.sh
@@ -36,7 +36,8 @@ git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-m
 git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting
 git clone --depth=1 https://github.com/romkatv/powerlevel10k.git ${ZSH_CUSTOM:-$HOME/.oh-my-zsh/custom}/themes/powerlevel10k
 
-cp ~/Git/confs/.zshrc ~
+cp ~/Git/confs/.zshrc ~/.zshrc
+
 
 
 #
